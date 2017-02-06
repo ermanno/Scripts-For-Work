ea50bea1a3bcad31d4f3299fc54f0a2f41bff53b
Ermanno Moser <Ermanno.Moser@trilogis.it>
2017-02-06 17:27:08 +0100

    updated calculation

--- generate_jasper_report.py
+++ generate_jasper_report.py
@@ -7,14 +7,19 @@ image_code_snippet = """
 </image>
 """
 
-starting_x = 10
-starting_y = 10
+current_x = 10
+current_y = 10
 img_width = 100
 img_height = 100
 watermark_width = 553
 watermark_height = 726
-interval_width = 10
-interval_height = 10
+
+watermark_width_without_borders = watermark_width - 2 * current_x
+watermark_height_witohut_borders = watermark_height - 2 * current_y
+num_of_columns = watermark_width_without_borders / img_width
+num_of_rows = watermark_height_witohut_borders / img_height
+interval_width = watermark_width_without_borders - img_width * (num_of_columns - 1)
+interval_height = watermark_height_witohut_borders - img_height * (num_of_rows - 1)
 
 def get_next_x(current_x):
     return current_x + img_width + interval_width
