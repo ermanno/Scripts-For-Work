5a2d6ba8f54aaefdd116b229f5f967dbe1dd2b11
Ermanno Moser <Ermanno.Moser@trilogis.it>
2017-02-07 09:36:17 +0100

    fixed problems with computation

--- generate_jasper_report.py
+++ generate_jasper_report.py
@@ -18,14 +18,8 @@ watermark_width_no_borders = watermark_width - 2 * current_x
 watermark_height_no_borders = watermark_height - 2 * current_y
 num_of_columns = watermark_width_no_borders / img_width
 num_of_rows = watermark_height_no_borders / img_height
-interval_width = watermark_width_no_borders - img_width * (num_of_columns - 1)
-interval_height = watermark_height_no_borders - img_height * (num_of_rows - 1)
-
-def get_next_x(current_x):
-    return current_x + img_width + interval_width
-
-def get_next_y(current_y):
-    return current_x + img_height + interval_width
+interval_width = (watermark_width_no_borders - img_width * num_of_columns) / (num_of_columns - 1)
+interval_height = (watermark_height_no_borders - img_height * num_of_rows) / (num_of_rows - 1)
 
 def next_y(y):
     return y + img_height + interval_height
